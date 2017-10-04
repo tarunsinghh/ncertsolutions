7dc8445f5b32d97cd7481f4951818ede172c6e80
TPS <tarunprakashiit@gmail.com>
2017-10-04 15:14:00 +0530

    traingles exercise class 9 half done

--- resources/views/blog/layouts/triangles.blade.php
+++ resources/views/blog/layouts/triangles.blade.php
@@ -13,15 +13,12 @@
 
 <p><strong><em>Exercise 7.1</em></strong></p>
 
-
-
-
-
-
 <p><strong><em>1.In quadrilateral ACBD, AC = AD and AB bisects ∠ A(see Fig. 7.16). Show that ∆ ABC ≅ ∆ ABD.
 What can you say about BC and BD?</em></strong></p>
 
 <p><strong><em>Solution:</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-1.png" alt="8" width="400" height="300" /></p>
+
 <p>In ∆ ABC and  ∆ ABD </p>
 <p>AC = AD</p>
 <p>CAB = DAB</p>
@@ -38,6 +35,8 @@ What can you say about BC and BD?</em></strong></p>
 (ii) BD = AC
 (iii) ∠ ABD = ∠ BAC.</strong></em>
 <p><strong><em>Solution:</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-2.png" alt="8" width="400" height="300" /></p>
+
 <p>This is simple too:</p>
 <p>In ∆ ABD and  ∆ BAC</p>
 <p>AD = BC (Given)</p>
@@ -46,7 +45,7 @@ What can you say about BC and BD?</em></strong></p>
 <p>So by SAS congruene condition:</p>
 <p>∆ ABC ≅ ∆ ABD</p>
 <p>Since triangles are congruent ,corresponding sides and angles will be  equal</p>
-<p>Hence,by CPCT</p>
+<p>Hence,by By C.P.C.T</p>
 <p> BD = AC</p>
 <p>∠ ABD = ∠ BAC</p>
 
@@ -58,6 +57,8 @@ What can you say about BC and BD?</em></strong></p>
 segment AB (see Fig. 7.18). Show that CD bisects
 AB.</strong></em>
 <p><strong><em>Solution:</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-3.png" alt="8" width="400" height="300" /></p>
+
 <p>This is simple too:</p>
 <p>In ∆ BOC and  ∆ AOD</p>
 <p>CBO = DAO (Given)</p>
@@ -66,7 +67,7 @@ AB.</strong></em>
 <p>So by AAS congruene condition:</p>
 <p>∆ BOC ≅ ∆ AOD</p>
 <p>Since triangles are congruent ,corresponding sides and angles will be  equal</p>
-<p>Hence,by CPCT</p>
+<p>Hence,by By C.P.C.T</p>
 <p> CO = OD</p>
 <p> BO = OA </p>
 <p>Since the intersection point of both lines O divided AB and  CD into 2 equal parts</p>
@@ -79,6 +80,8 @@ AB.</strong></em>
 another pair of parallel lines p and q
 (see Fig. 7.19). Show that ∆ ABC ≅ ∆ CDA.</strong></em>
 <p><strong><em>Solution:</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-4.png" alt="8" width="400" height="300" /></p>
+
 <p>In ∆ ABC and  ∆ CDA</p>
 <p>BCA = DAC (Alternate interior angle as l||m)</p>
 <p>BAC = DCA (Alternate interior angle as l||m)</p>
@@ -107,6 +110,8 @@ of ∠ A.</strong></em></p>
 <p><strong><em>6. In Fig. 7.21, AC = AE, AB = AD and
 ∠ BAD = ∠ EAC. Show that BC = DE.</strong></em></p>
 <p><strong><em>Solution:</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-5.png" alt="8" width="400" height="300" /></p>
+
 <p>We are given two things but need a third thing too to prove triangles are congruent</p>
 <p>∠ BAD = ∠ EAC</p>
 <p>∠ BAD + ∠ DAC  = ∠ EAC + ∠ DAC</p>
@@ -122,8 +127,9 @@ of ∠ A.</strong></em></p>
 
 <p><strong>Question 7:</strong></p>
 <p><strong><em>AB is a line segment and p is its mid-point. D and E are points on the same side of AB such that \(∠ BAD = ∠ ABE\ and\ ∠ EPA = ∠ DPB\) (see fig.). Show that</em></strong></p>
-<p><strong><em>            (i)  DAP = ∆ EBP</em></strong></p>
+<p><strong><em>            (i)  DAP ≅ ∆ EBP</em></strong></p>
 <p><strong><em>            (ii) AD = BE</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-7.png" alt="8" width="400" height="300" /></p>
 <p><strong><em>Solution:</em></strong></p>
 <p>We are given two things directly and one thing we can derive using given information</p>
 <p>∠ EPA = ∠ DPB </p>
@@ -136,7 +142,7 @@ of ∠ A.</strong></em></p>
 <p>∠ EPB = ∠ DPA</p>
 <p>AP = BP (P is the midpoint of the line segment AB)</p>
 <p>so by AAS congruence condition</p>
-<p>∆ DPA = ∆ EPB</p>
+<p>∆ DPA ≅ ∆ EPB</p>
 <p>AD = BE  (By C.P.C.T)</p>
 <p>Hence Proved.</p>
 
@@ -144,54 +150,203 @@ of ∠ A.</strong></em></p>
 
 <p><strong>Question 8:</strong></p>
 <p><strong><em>In right triangle ABC, right angles at C, M is the mid-point of hypotenuse AB, C is joined to M and produced to a point D such that DM = CM. Point D is joined to point B (see fig.). Show that :</em></strong></p>
-<p><strong><em>            (i) ∆ AMC = ∆ BMD</em></strong></p>
+<p><strong><em>            (i) ∆ AMC ≅ ∆ BMD</em></strong></p>
 <p><strong><em>            (II) ∠ DBCis a right angle.</em></strong></p>
-<p><strong><em>            (iii) ∆ DBC = ∆ ACB</em></strong></p>
+<p><strong><em>            (iii) ∆ DBC ≅ ∆ ACB</em></strong></p>
 <p><strong><em>            (iv) \( CM= \frac{1}{2} AB \)</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-1-8.png" alt="8" width="400" height="300" /></p>
 <p><strong>Solution:</strong></p>
 <p>In ∆ BMB and ∆ DMC , we have</p>
 <p>(i) DM = CM (given)</p>
 <p>BM = AM (M is the midpoint of AB)</p>
 <p>∠ DMB = ∠ AMC (Vertically opposite angles)</p>
-<p>∆ AMC = ∆ BMD (By SAS)</p>
+<p>∆ AMC ≅ ∆ BMD (By SAS)</p>
 <p>Hence Proved.</p>
 <p>(ii) AC || BD ( ∠ DBM\ and\ ∠ CAM are .alternate angles)</p>
 <p> ∠ DBC + ∠ ACB = \( 180^{o}\) (Sum of co-interior angles)</p>
 <p>∠ DBC = \( 90^{o}\)</p>
 <p>Hence proved.</p>
 <p>(iii) In ∆ DBC and ∆ ACB , We have</p>
-<p>DB = AC (CPCT)</p>
+<p>DB = AC (By C.P.C.T)</p>
 <p>BC = BC (Common)</p>
 <p>∠ DBC = ∠ ACB (Each = \( 90^{o}) \) )</p>
-<p>∆ DBC = ∆ ACB  (By SAS)</p>
+<p>∆ DBC ≅ ∆ ACB  (By SAS)</p>
 <p>Hence proved.</p>
 <p>(iv) AB = CD</p>
-<p>\( \frac{1}{2} AB = \frac{1}{2} CD\) (CPCT)</p>
+<p>\( \frac{1}{2} AB = \frac{1}{2} CD\) (By C.P.C.T)</p>
 <p>Hence, \(\frac{1}{2} AB = CM\) Proved.</p>
 
-<div id="disqus_thread"></div>
-<script>
-   
-    var disqus_config = function () {
-        //this.page.url = 'http://www.ncertsolved.com/solutions/quadratic-equations';
-        
-        this.page.url = document.url;
-        console.log(this.page.url);
-        this.page.identifier = this.page.url+'fff';
-        console.log(this.page.identifier);
-        this.page.title = '123';
-    };
-    
-    (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
-        var d = document, s = d.createElement('script');
-        
-        s.src = '//ncertsolved-com.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
-        
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
+
+<p><strong><em>Exercise 7.2:</em></strong></p>
+<p><strong>Question 1:</strong></p>
+<p><strong><em>In an isosceles triangle ABC, with AB = AC, the bisectors of ∠ B and ∠ C intersect
+each other at O. Join A to O. Show that :
+(i) OB = OC
+(ii) AO bisects ∠ A</em></strong></p>
+
+<p><strong>Solution:</strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-1.png" alt="8" width="400" height="300" /></p>
+
+<p>We need to proove two things OB = OC,AO bisects ∠ A which is possible if we can show ∆ OAB ≅ ∆ OAC </p>
+<p>Now AB = AC ,we are given,OA =OA will become common we need a third thing </p>
+<p>Now,∠ ABC = ∠ ACB</p>
+<p>\(\frac{1}{2}∠ ABC=\frac{1}{2}∠ ACB \)</p>
+<p>\(∠ ABO=∠ ACO\) </p>
+
+<p>In  ∆ AOB and ∆ AOC, we have</p>
+<p>OA=OA (Common)</p>
+<p>∠ XYO = ∠ XZO         (proved above)</p>
+<p>  AB =AC (Given)  </p>
+
+<p>∆ AOB  ≅ ∆ AOC          (SAS congruence)</p>
+<p>So by C.P.C,T</p>
+<p>OB = OC</P>
+<p>∠ BA0 = ∠ 0AC</p>
+<p>Hence AO bisects ∠ A</p>
+
+
+<p><strong>Question 2:</strong></p>
+<p>In ∆ ABC, AD is the perpendicular bisector of BC
+(see Fig. 7.30). Show that ∆ ABC is an isosceles
+triangle in which AB = AC.
+<p><strong> Solution:</strong></p>
+
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-2.png" alt="8" width="400" height="300" /></p>
+<p>In ∆ ABD≅ ∆ ACD , we have</p>
+<p>∠ ADB =∠ ADC    (each =90°)</p>
+<p>AD= DC ( AD bisects BC)</p>
+<p>AD = AD (common)</p>
+<p> ∆ ABD=∆ ACD   (SAS)</p>
+<p> AB=AC    (By C.P.C.T)</p>
+<p>Hence, ∆ ABC  is an isosceles triangle .<strong>Proved</strong></p>
+
+
+
+<p><strong><em>Question 3:</em></strong></p>
+<p><strong><em>ABC is an isosceles triangle in which altitudes BE and CF are drawn to equal sides AC and</em></strong></p>
+<p><strong><em>AB respectively (see figure).Show that these altitudes are equal.</em></strong></p>
+<p><strong>Solution:</strong></p>
+
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-3.png" alt="8" width="400" height="300" /></p>
+
+<p>All the  questions of this chapter will be like same,you will be given two things equal and you will need a third thing</p>
+<p>Which you will derive from that extra information informtion given in the question</p>
+<p>In ∆ ABC</p>
+<p>AB=AC   (Given)</p>
+<p>∠ B=∠ C     (angles opposite to equal sides of a triangle are equal)</p>
+<p>Now in right triangles BFC and CEB ,</p>
+<p>∠ BFC=∠ CEB                 (Each = 90° )</p>
+<p>∠ B=∠ C                 (proved above)</p>
+<p>BC=BC     ( common)</p>
+<p>∆ BFC ≅ ∆ CEB             (AAS)</p>
+<p>Hence, BE = CF (By C.P.C.T) <strong>proved</strong></p>
+
+
+<p><strong>Question 4:</strong></p>
+<p><strong><em>ABC is a triangle in which altitudes BE and CF to sides AC and AB are equal (see.fig) show that</em></strong></p>
+<p>(1) ∆ ABE ≅ ∆ ACF </p>
+<p>(2) AB=AC   i,e ABC is an isosceles triangle.</p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-4.png" alt="8" width="400" height="300" /></p>
+
+<p><strong><em>Solution</em></strong></p>
+<p>In ∆ ABE  and ACF </p>
+<p>∠ BAE=∠ CAF  (common)</p>
+<p>∠ BEA=∠ CFA    ( Each =90°)</p>
+<p>BE=CF         (Given)</p>
+<p>So, ∆ ABE ≅  ∆ ACF        (AAS)proved</p>
+<p>  AB=AC                 ( By C.P.C.T)</p>
+<p>hence  ABC is an isosceles triangle <strong>Proved.</strong></p>
+
+
+
+<p><strong>Question 5:</strong></p>
+<p><strong><em>ABC and DBC are two isosceles triangles on the same base BC (see Fig. 7.33). Show that
+∠ ABD = ∠ ACD.</em></strong></p>
+
+<p>Solution. In isosceles ∆ ABC, We have</p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-5.png" alt="8" width="400" height="300" /></p>
+
+<p>This is a very easy question,we have to just use angle opposite to equal sides are equal and then add the two angles</p>
+<p>AB= AC</p>
+<p>∠ ABC=∠ ACB    ---- (1)</p>
+<p>(Angles opposite to equal sides are equal)</p>
+<p>Now , in isosceles ∆ DCB</p>
+<p>BD=CD</p>
+<p>∠ DBC=∠ DCB  --------(2)</p>
+<p>(Angle opposite to equal sides are equal)</p>
+
+<p>Adding (1) and (2)  , we have</p>
+<p>∠ ABC+∠ DBC=∠ ACB+∠ DCB</p>
+<p>∠ ABD=∠ ACD .<strong>proved</strong></p>
+
+
+<p><strong>Question 6:</strong></p>
+<p><strong><em>∆ABC is an isosceles triangle in which AB = AC.Side BA is produced to D such that AD = AB
+(see Fig. 7.34). Show that ∠ BCD is a right angle.</em></strong></p>
+
+<p><strong>Solution:</strong></p>
+<p><strong><em>This is a good question,and you should try to learn from this</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-6.png" alt="8" width="400" height="300" /></p>
+
+<p>AB=AC </p>
+<p>∠ ACB= ∠ ABC        -(1)</p>
+<p>(Angles opposite to equal sides are equal)</p>
+<p>AB=AD          (Given)</p>
+<p>AD=AC             (AB=AC)</p>
+<p>∴ ∠ ACD=∠ ADC    (2)</p>
+<p>(Angles opposite to equal sides are equal)</p>
+<p>Adding (i) and (ii)</p>
+<p>∠ ACB+∠ ACD=∠ ABC+∠ ADC</p>
+<p>∠ BCD=∠ ABC+∠ ADC</p>
+<p>Now in ∆ BCD ,We have\)</p>
+<p>∠ BCD+∠ DBC+∠ BDC = 180  (Angle sum property of a triangle)</p>
+<p> ∠ BCD+∠ BCD=180°</p>
+<p>2∠ BCD=180 </p>
+<p>∠ BCD= 90 </p>
+
+
+<p><strong>Question 7:</strong></p>
+<p><strong><em>ABC is a right angled triangle in which ∠ A=90 and AB=AC.Find ∠ B and ∠ C.</em></strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-7.png" alt="8" width="400" height="300" /></p>
+
+<p><strong><em>Solution</em></strong></p>
+
+<p>In ∆ ABC</p>
+
+<p>∠ A =90</p>
+<p>AB =AC</p>
+<p>We know that angles opposite to equal sides of an isosceles triangle are equal</p>
+<p>So, ∠ B=∠ C </p>
+<p>Since ∠ A=90 , therefore sum of remaining two angles =90</p>
+<p>Hence ∠ B + ∠ C= 90 </p>
+<p>Hence 2∠ B  = 90 </p>
+<p>∠ B  = 45 = ∠ C </p>
+
+
+<p><em><strong>Question 8:</strong></em></p>
+<p><em><strong>Show that the angles of an equilateral triangle are 60 each.</strong></em></p>
+<p><strong>Solution:</strong></p>
+<p><img class="alignnone wp-image-2112" src="/images/9/0/triangles/9-0-7-2-8.png" alt="8" width="400" height="300" /></p>
+
+<p>As ∆ ABC  is an equilateral</p>
+<p>So, AB=BC=AC</p>
+<p>Now, AB= AC </p>
+<p> ∠ ACB=∠ ABC               -(1)( angles opposite to equal side are equal)</p>
+<p>Again BC=AC</p>
+<p> ∠ BAC=∠ ABC         (2)    (same reason)</p>
+<p>Now in ∆ ABC\)</p>
+<p>\(∠ ABC+∠ ACB+∠ BAC=180\)  (angle sum property of a triangle are equal)</p>
+<p>∠ ABC+∠ ACB+∠ BAC = 180 (from (1) (2))</p>
+<p>3∠ ABC =180</p>
+<p>∠ ABC =180/3=60°</p>
+<p>Also from (i) and (ii)</p>
+<p>∠ ACB =60 and ∠ BAC = 60 </p>
+<p>Hence each angle of an equilateral triangle is 60°  <strong>Proved</strong></p>
+
+
+
+
 
 @stop
 
