7b4049a2d0071d117a6c1b1eaa1efab76006947a
TPS <tarunprakashiit@gmail.com>
2017-11-12 13:10:33 +0530

    2 exercie done polynomial class 9

--- resources/views/blog/layouts/polynomial-9.blade.php
+++ resources/views/blog/layouts/polynomial-9.blade.php
@@ -96,6 +96,156 @@ Classify the following as linear, quadratic and cubic polynomials:
 <p>&nbsp;</p>
 
 
+<p><strong>EXERCISE – 2.1</strong></p>
+
+
+<p><strong>Question 1:<br/>
+Find the value of the polynomial \( 5x – 4x^{2} + 3 \) at
+(i) x = 0
+(ii) x = –1
+(iii) x = 2?.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p><strong>Answer:</strong></p>
+<p>Let \( f(x)= 5x -4x^{2}+ 3\)</p>
+<p><strong>(i)</strong> When a=0</p>
+<p>\( f(0) = 5(0) + 4(0)^{2} + 3= 3 \)</p>
+<p><strong><em>(ii) </em></strong>When a=-1</p>
+<p> \( f(x) = 5x + 4x^{2}+ 3\\ f(-1) = 5(-1) + 4(-1)^{2} + 3 = -5 &#8211; 4 + 3 = -6\) </p>
+<p><strong><em>(iii) </em></strong>When a=2</p>
+<p> \( f(x) = 5x + 4x^{2} + 3\\ f(2) = 5(2) + 4(2)^{2} + 3 = 10 &#8211; 16 + 3 = -3\) </p>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 2:<br/>
+Find p(0), p(1) and p(2) for each of the following polynomials:
+(i) p(y) =  \(y^{2} – y + 1 \)
+(ii) p(t) = \( 2 + t + 2t^{2} – t^{3} \)
+(iii) p(x) = \( x^{3} \)
+(iv) p(x) = (x – 1) (x + 1)?.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p><strong> \((i) p(y) = y2 &#8211; y + 1\\ ∴ p(0) = (0)^{2}- (0) + 1 = 1\\ p(1) = (1)^{2} &#8211; (1) + 1 = 1\\ p(2) = (2)^{2} &#8211; (2) + 1 = 3\)</p>
+<br>
+<p><strong><em> (ii) </em></strong><strong><em>\( p(x) = 2 + t + 2t^{2}- t^{3}\)</em></strong></p>
+<p><strong>\( p(a) = 2 + t + 2t^{2}-t^{3}\\ ∴ p(0) = 2 + 0 + 2 (0)^{2} &#8211; (0)^{3} = 2\\ p(1) = 2 + 1+ 2(1)^{2} &#8211; (1)^{3} = 2 + 1 + 2 &#8211; 1 = 4\\ p(2) = 2 + 2 + 2(2)^{2} &#8211; (2)^{3} = 2 + 2 + 8 &#8211; 8 = 4\)</p>
+<br>
+<p><strong><em>(iii) \( p(x) = x^{3}\)</em></strong></p>
+<p><strong>\( p(x) = x3\\ ∴ p(0) = (0)^{3}= 0\\ p(1) = (1)^{3} = 1\\ p(2) = (2)^{3} = 8\)</p>
+<br>
+<p><strong><em>(iv)</em></strong> <strong><em>\( p(x) = (x- 1) (x+ 1)\)</em></strong></p>
+<p><strong>\(p(x) = (x &#8211; 1) (x + 1)\\ ∴ p(0) = (0 &#8211; 1) (0 + 1) = (- 1) (1) = &#8211; 1\\ p(1) = (1 &#8211; 1) (1 + 1) = 0 (2) = 0\\ p(2) = (2 &#8211; 1 ) (2 + 1) = 1(3) = 3\)</p>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 3:<br/>
+Verify whether the following are zeroes of the polynomial, indicated against them.
+<p>(i) p(x) = 3x + 1, x = \( \frac{–1}{3} \) </p>
+<p>(ii) p(x) = 5x – π, x = \( \frac{4}{5} \) </p>
+<p>(iii) p(x) = \( x^{2} – 1, x = 1, –1 \)</p>
+<p>(iv) p(x) = (x + 1) (x – 2), x = – 1, 2</p>
+<p>(v) p(x) = x^{2},x = 0 </p> 
+<p>(vi) p(x) = lx + m, x = \( \frac{–m}{l} \) </p>
+<p>(vii)p(x) = 3x^{2} - 1, x = \( \frac{–1}{\sqrt{3}},  \frac{2}{\sqrt{3}} \)
+<p>(viii) p(x) = 2x + 1, x = 2 ?</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+
+<p>i) For, \(x = -\frac{1}{3}\)</p>
+<p>\(p(x) = 3x + 1\\ ∴ p(-\frac{1}{3}) = 3(-\frac{1}{3}) + 1=-1+1=0\)</p>
+<p>\(∴ -\frac{1}{3}\) is a zero of p(x).</p>
+<br>
+<p><strong><em> </em></strong><strong><em>(ii) \(p(x) = 5x &#8211; \pi , x = \frac{4}{5}\)</em></strong></p>
+<p>For, \(x = \frac{4}{5}\)</p>
+<p>\(p(x) = 5x &#8211; \pi \\ ∴ p(\frac{4}{5}) = 5(\frac{4}{5}) &#8211; \pi=4-\pi\)</p>
+<p>\(∴ \frac{4}{5}\) is not a zero of p(x).</p>
+<br>
+<p><strong><em> </em></strong><strong><em>(iii) \(p(x) = x^{2}- 1, x = 1, -1\)</em></strong></p>
+<p>For, \(x = 1,-1\)</p>
+<p>\(p(x) = x^{2}- 1\\ ∴ p(1) = x^{1}- 1=1-1=0\\ p(-1) = x^{-1}- 1=1-1=0\)</p>
+<p>\(∴ 1,-1\) are zeros of p(x).</p>
+<p>&nbsp;</p>
+<p><strong><em>(iv)</em></strong> <strong><em>\(p(x) = (x + 1) (x &#8211; 2), x = -1, 2\)</em></strong></p>
+<p>For, \(x = -1,2\)</p>
+<p>\(p(x) = (x + 1) (x &#8211; 2)\\ ∴ p(-1) = (-1 + 1) (-1 &#8211; 2)\\ =((0)(-3))\\ =0\\ \\ p(2) = (2 + 1) (2 &#8211; 2)\\ =(3)(0)=0\)</p>
+<p>\(∴ -1,2\) are zeros of p(x).</p>
+
+
+<p><strong><em>(v) \(p(x) = x^{2}, x = 0\)</em></strong></p>
+<p>For, \(x = 0\)</p>
+<p>\(p(x) = 0^{2}=0\)</p>
+<p>\(∴ 0\) is a  zero of p(x).</p>
+<p>&nbsp;</p>
+<p><strong><em>(vi)</em></strong> <strong><em>\(p(x)=lx+m,x=-\frac{m}{l}\)</em></strong></p>
+<p>For, \( x=-\frac{m}{l} \)</p>
+<p>\(p(x)=lx+m\\ ∴ p(-\frac{m}{l} )=l(-\frac{m}{l} )+m=-m+m=0\)</p>
+<p>\(∴-\frac{m}{l}\) is a zero of p(x).</p>
+
+
+<p><strong><em>(vii)</em></strong> <strong><em>\(p(x)=3x^{2}-1,x=-\frac{1}{\sqrt{3}},\frac{2}{\sqrt{3}}\)</em></strong></p>
+<p>For, \( x=-\frac{1}{\sqrt{3}},\frac{2}{\sqrt{3}} \)<br />
+<p>\(p(x)=3x^{2}-1\\ ∴ p(-\frac{1}{\sqrt{3}})=3(-\frac{1}{\sqrt{3}})^{2}-1\\ =3(\frac{1}{3})-1=1-1=0\\ \\ ∴ p(\frac{2}{\sqrt{3}})=3(\frac{2}{\sqrt{3}})^{2}-1\\ =3(\frac{4}{3})-1=4-1=3\neq 0\)</p>
+<p>\(∴ -\frac{1}{\sqrt{3}}\) is a zero of p(x) but \(\frac{2}{\sqrt{3}}\) is not a zero of p(x).</p>
+
+
+
+<p><strong><em>(viii) \(p(x) = 2x + 1, x = \frac{1}{2}\)</em></strong><strong><em><br />
+<p>For, \( x=\frac{1}{2} \)</p>
+<p>\( p(x) = 2x + 1\\ ∴ p(\frac{1}{2} ) = 2(\frac{1}{2}) + 1=1+1=2\neq 0\)
+<p>\(∴\frac{1}{2}  \) is not a  zero of p(x).</p>
+
+
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 4:<br/>
+Find the zero of the polynomial in each of the following cases:
+(i) p(x) = x + 5 (ii) p(x) = x – 5 (iii) p(x) = 2x + 5
+(iv) p(x) = 3x – 2 (v) p(x) = 3x (vi) p(x) = ax, a ≠ 0
+(vii) p(x) = cx + d, c ≠ 0, c, d are real numbers.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+
+<p><strong><em>(i)p(x) = x + 5 </em></strong></p>
+<p>\( p(x) = x + 5 \\ \Rightarrow x + 5=0 \Rightarrow x=-5\)</p>
+<p>\(∴\)-5 is a zero polynomial of the polinomila p(x).</p>
+<p>&nbsp;</p>
+<p><strong><em>(ii) p(x) =</em></strong><strong><em> x – 5</em></strong></p>
+<p>\( p(x) = x -5 \\ \Rightarrow x -5=0 \Rightarrow x=5\)</p>
+<p>\(∴\)5 is a zero polynomial of the polinomila p(x).</p>
+<p>&nbsp;</p>
+<p><strong><em>(iii) p(x) = 2x</em></strong><strong><em> + 5</em></strong></p>
+<p> \( p(x) =2 x + 5 \\ \Rightarrow 2x + 5=0 \Rightarrow 2x=-5\\ \Rightarrow x=\frac{-5}{2} \)</p>
+<p>\(∴\)\( x=\frac{-5}{2} \) is a zero polynomial of the polynomial p(x).<strong><em><br />
+</em></strong></p>
+<p>&nbsp;</p>
+<p><strong><em>(iv)p(x) = 3x &#8211; 2 </em></strong></p>
+<p>\( p(x) =3 x &#8211; 2 \\ \Rightarrow 3x -2=0 \Rightarrow 3x=2\\ \Rightarrow x=\frac{2}{3} \)</p>
+<p>\(∴\)\( x=\frac{2}{3} \) is a zero polynomial of the polynomial p(x).</p>
+<p>&nbsp;</p>
+<p><strong> </strong><strong><em>(v)p(x) = 3x </em></strong></p>
+<p> \( p(x) =3 x \\ \Rightarrow 3x =0 \Rightarrow x=0 \)</p>
+<p>\(∴\)0 is a zero polynomial of the polynomial p(x).</p>
+<p>&nbsp;</p>
+
+
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+
+
+
+
 
 
 
