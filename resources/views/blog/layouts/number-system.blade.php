997faa190c60751af7ef269d34b83f3ec1b6047d
TPS <tarunprakashiit@gmail.com>
2017-11-29 13:03:20 +0530

    3 4 exercise completed number syayte  class 9

--- resources/views/blog/layouts/number-system.blade.php
+++ resources/views/blog/layouts/number-system.blade.php
@@ -107,6 +107,354 @@ number line at P,then P represents \( \sqrt{5}     \) on number line </p>
 <p>&nbsp;</p>
 
 
+<p><strong>EXERCISE – 1.3</strong></p>
+
+
+<p><strong>Question 1:<br/>
+ Write the following questions in decimal form and mention what kind of decimal expansion each Shas:</strong><br />
+(i) \(\frac{36}{100}\)</p>
+<p><strong> </strong>= 0.36 (Terminating)</p>
+<p>(ii) \(\frac{1}{11}\)</p>
+<p>0.09090909&#8230; = 0.9 (Non terminating and repeating)</p>
+<p>(iii) \(4\frac{1}{8}\)<br />
+= 33/8 = 4.125 (Terminating)</p>
+<p>(iv) \(\frac{3}{13}\)<br />
+= 0.230769230769&#8230; = 0.230769 (Non terminating and repeating)</p>
+<p>(v) \(\frac{2}{11}\)<br />
+= 0.181818181818&#8230; = 0.18 (Non terminating and repeating)</p>
+<p>(vi) \(\frac{329}{400}\)<br />
+= 0.8225 (Terminating)</p>
+<p>&nbsp;</p>
+
+
+
+
+<p><strong>Question 2:<br/>
+<p>We know that 1/7 = 0.142857. Can you predict what the decimal expansion of 2/7, 3/7, 4/7, 5/7, 6/7 are without actually doing the long division? If so, how?</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>Yes. We can be done this by:</p>
+<p>\(\frac{2}{7}=2\times \frac{1}{7}=2\times 0.\overline{142857}=\overline{285714}\)</p>
+<p>\(\frac{3}{7}=3\times \frac{1}{7}=3\times 0.\overline{142857}=\overline{428571}\)</p>
+<p>\(\frac{4}{7}=4\times \frac{1}{7}=4\times 0.\overline{142857}=\overline{571428}\)</p>
+<p>\(\frac{5}{7}=5\times \frac{1}{7}=5\times 0.\overline{142857}=\overline{714285}\)</p>
+<p>\(\frac{6}{7}=6\times \frac{1}{7}=6\times 0.\overline{142857}=\overline{857142}\)</p>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 3:<br/>
+Express the following in the form </strong><em><strong>p</strong></em><strong>/</strong><em><strong>q</strong></em><strong>where </strong><em><strong>p</strong></em><strong>and </strong><em><strong>q</strong></em><strong> are integers and </strong><em><strong>q</strong></em><strong> ≠ 0.</strong></p>
+<p>(i) 0. \(\overline{6}\)</p>
+<p>(ii) 0.4\(\overline{7}\)</p>
+<p>(iii) 0. \(\overline{001}\)</p>
+<p>&nbsp;</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>(i) 0. \(\overline{6}\)  = 0.666&#8230;</p>
+<p>Let <em>x</em> = 0.666&#8230;</p>
+<p>10<em>x</em> = 6.666&#8230;</p>
+<p>10<em>x</em> = 6 + <em>x</em></p>
+<p>9<em>x</em> = 6</p>
+<em>x</em> = 2/3</p>
+<p>(ii) 0.4\(\overline{7}\)</p>
+<p>= 0.4777&#8230;</p>
+<p>= 4/10 + 0.777/10</p>
+<p>Let <em>x</em> = 0.777…</p>
+<p>10<em>x</em> = 7.777…</p>
+<p>10<em>x</em> = 7 + <em>x</em></p>
+<p><em>x</em> = 7/9</p>
+<p>4/10 + 0.777&#8230;/10 = 4/10 + 7/90</p>
+<p>= 36/90 +7/90 = 43/90</p>
+<p>&nbsp;</p>
+<p>(iii) 0. \(\overline{001}\)</p>
+<p>= 0.001001&#8230;</p>
+<p>Let <em>x</em> = 0.001001&#8230;</p>
+<p>1000<em>x</em> = 1.001001…</p>
+<p>1000<em>x</em> = 1 + <em>x</em></p>
+<p>999<em>x</em> = 1</p>
+<p><em>x</em> = 1/999</p>
+</div>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 4:<br/>
+<p>Express 0.99999… in the form \( \frac{p}{q} \). Are you surprised by your answer? With your teacher and classmates discuss why the answer makes sense.</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>Let <em>x</em> = 0.9999…</p>
+<p>10<em>x</em> = 9.9999…</p>
+<p>10<em>x</em> = 9 + <em>x</em></p>
+<p>9<em>x</em> = 9</p>
+<p><em>x</em> = 1</p>
+<p>The difference between 1 and 0.999999 is 0.000001 which is negligible. Thus, 0.999 is too much near 1, Therefore, 1 as the answer can be justified.</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+
+<p><strong>Question 5:<br/>
+<p>What can be the maximum number of digits in the repeating block of digits in the decimal expansion of 1/17?Perform the division to check your answer..</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>\(\frac{1}{17} = 0.\overline{0588235294117647}\)<br />
+<p>here are 16 digits in the repeating block of the decimal expansion of \(\frac{1}{17}\)</p>
+<p>=\(0.\overline{0588235294117647}\)</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+
+<p><strong>Question 6:<br/>
+<p>Look at several examples of rational numbers in the form </strong><em><strong>p</strong></em><strong>/</strong><em><strong>q </strong></em><strong>(</strong><em><strong>q </strong></em><strong>≠ 0) where </strong><em><strong>p </strong></em><strong>and </strong><em><strong>q </strong></em><strong>are integers with no common factors other than 1 and having terminating decimal representations (expansions). Can you guess what property </strong><em><strong>q</strong></em><strong> must satisfy?</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>We observe that when q is 2, 4, 5, 8, 10&#8230; Then the decimal expansion is terminating. For example:<br />
+<p>1/2 = 0. \(\overline{5}\), denominator <em>q</em> = 2<sup>1</sup></p>
+<p>7/8 = 0. \(\overline{875}\), denominator <em>q</em> = 2<sup>3</sup></p>
+<p>4/5 = 0. \(\overline{8}\), denominator <em>q</em> = 5<sup>1</sup></p>
+<p>We can observe that the terminating decimal may be obtained in the situation where prime factorization of the denominator of the given fractions has the power of only 2 or only 5 or both.</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+
+<p><strong>Question 7:<br/>
+<p>Write three numbers whose decimal expansions are non-terminating non-recurring.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>Three numbers with decimal expansions that are non-terminating non-recurring are:</p>
+<p>0.303003000300003&#8230;</p>
+<p>0.505005000500005&#8230;</p>
+<p>0.7207200720007200007200000…</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 8:<br/>
+<p>Find three different irrational numbers between the rational numbers 5/7 and</strong></p>
+<p><strong>9/11.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>5/7 = 0. \(\overline{714285}\)</p>
+<p>9/11 = 0. \(\overline{81}\)</p>
+<p>Three different irrational numbers are:</p>
+<p>0.73073007300073000073…</p>
+<p>0.75075007300075000075…</p>
+<p>0.76076007600076000076…</p>
+</div>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 9:<br/>
+<p>Classify the following numbers as rational or irrational according to their type:</strong></p>
+<p><strong>(i) \(\sqrt{23}\)</strong></p>
+<p><strong>(ii) \(\sqrt{225}\)</strong></p>
+<p><strong>(iii) 0.3796</strong></p>
+<p><strong>(iv) 7.478478</strong></p>
+<p><strong>(v) 1.101001000100001…</strong></p>
+<p>&nbsp;</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+
+<p>(i) \(\sqrt{23}\)= 4.79583152331&#8230;</p>
+<p>Since the number is non-terminating non-recurring therefore, it is an irrational number.</p>
+<p>&nbsp;</p>
+<p>(ii) \(\sqrt{225}\)= 15 = 15/1<br />
+Since the number is rational number as it can represented in <em>p</em>/<em>q </em>form.</p>
+<p>&nbsp;</p>
+<p>(iii) 0.3796</p>
+<p>Since the number is terminating therefore, it is an rational number.</p>
+<p>&nbsp;</p>
+<p>(iv) 7.478478 = 7.478</p>
+<p>Since the this number is non-terminating recurring, therefore, it is a rational number.</p>
+<p>&nbsp;</p>
+<p>(v) 1.101001000100001…</p>
+<p>Since the number is non-terminating non-repeating, therefore, it is an irrational number.</p>
+</div>
+<p>&nbsp;</p>
+
+
+<p><strong>EXERCISE – 1.1</strong></p>
+<p><strong>Question 1:<br/>
+<p>Classify the following numbers as rational or irrational:</strong></p>
+<p>(i) 2 &#8211; \(\sqrt{5}\)</p>
+<p>(ii) (3 + \(\sqrt{23}\)) &#8211; \(\sqrt{23}\)</p>
+<p>(iii) 2\(\sqrt{7}\)/7\(\sqrt{7}\)</p>
+<p>(iv) 1/\(\sqrt{2}\)</p>
+<p>(v) 2π</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>(i) 2 &#8211; \(\sqrt{5}\) = 2 &#8211; 2.2360679… = &#8211; 0.2360679…</p>
+<p>Since the number is is non-terminating non-recurring therefore, it is an irrational number.</p>
+<p>(ii) (3 + \(\sqrt{23}\)) &#8211; √23 = 3 + √23 &#8211; √23 = 3 = 3/1<br />
+Since the number is rational number as it can represented in <em>p</em>/<em>q </em>form.</p>
+<p>(iii) 2\(\sqrt{7}\)/7\(\sqrt{7}\) = 2/7<br />
+Since the number is rational number as it can represented in <em>p</em>/<em>q </em>form.</p>
+<p>(iv) 1/√2 = \(\sqrt{2}\)/2 = 0.7071067811&#8230;<br />
+Since the number is is non-terminating non-recurring therefore, it is an irrational number.</p>
+<p>(v) 2π = 2 × 3.1415… = 6.2830…<br />
+Since the number is is non-terminating non-recurring therefore, it is an irrational number.</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 2:<br/>
+<p>Simplify each of the following expressions:</strong></p>
+<p><strong>(i) (3 + \(\sqrt{3}\)) (2 + \(\sqrt{2}\))</strong></p>
+<p><strong>(ii) (3 + \(\sqrt{3}\)) (3 &#8211; √3)</strong></p>
+<p><strong>(iii) (\(\sqrt{5}\) + \(\sqrt{2}\))<sup>2</sup></strong></p>
+<p><strong>(iv) (\(\sqrt{5}\) &#8211; \(\sqrt{2}\)) (\(\sqrt{5}\) + \(\sqrt{2}\))</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>(i) (3 + \(\sqrt{3}\)) (2 + \(\sqrt{2}\))</p>
+<p>3 × 2 + 2 + \(\sqrt{3}\) + 3\(\sqrt{2}\) + √3 ×\(\sqrt{2}\)</p>
+<p>6 + 2\(\sqrt{3}\)+3\(\sqrt{2}\)+ √6</p>
+<p>(ii) (3 + \(\sqrt{3}\)) (3 &#8211; \(\sqrt{3}\))</p>
+<p>=  \(3^{2}-(\sqrt{3})^{2}\)<br />
+= 9 &#8211; 3</p>
+<p>= 6</p>
+<p>(iii) (\(\sqrt{5}\) + \(\sqrt{2}\)) \(^{2}\)</p>
+<p>= \((\sqrt{5})^{2}\)+\((\sqrt{2})^{2}\) + 2 ×√5 × √2<br />
+= 5 + 2 + 2 × \(\sqrt{5}\)× 2</p>
+<p>= 7 +2 + \(+ \sqrt{10}\)</p>
+<p>(iv) (\(\sqrt{5}\)&#8211; \(\sqrt{2}\)) (\(\sqrt{5}\)+ \(\sqrt{2}\))</p>
+<p>= \((\sqrt{5})^{2}\)&#8211; \((\sqrt{2})^{2}\)</p>
+<p>= 5 &#8211; 2</p>
+<p>= 3.</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 3:<br/>
+<p>π is defined as the ratio of the circumference (say c) of a circle to its diameter (D). That is, π = c/d. This seems to contradict the fact that π is irrational. How will you resolve this contradiction?</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>There is no contradiction. When we measure a value with a scale, we only obtain an approximate value. We never obtain an exact value. Therefore, we may not realize whether c or d is irrational. The value of π is almost equal to 22/7 or 3.142857&#8230;</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+<p><strong>Question 4:<br/>
+<p>Represent \((\sqrt{9.3})^{2}\)&#8211; on the number line.</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+Step 1: Draw a line segment of 9.3 units. Extend it to C so that BC is forms of 1 unit.<br />
+Step 2: Now, AC = 10.3 units. Find the centre of AC and name it as O.<br />
+Step 3: Draw a semi-circle with radius OC and centre O.<br />
+Step 4: Draw a perpendicular line BD to AC at point B which intersect the semicircle at D. Also, Join OD.<br />
+Step 5: Now, OBD is a right angled triangle.<br />
+Here, OD = 10.3/2 (radius of semi circle), OC = 10.3/2, BC = 1<br />
+OB = OC – BC = (10.3/2) – 1 = 8.3/2<br />
+Using Pythagoras theorem,<br />
+\(OD^{2}=BD^{2}+OB^{2}\)</p>
+\((10.3/2)^{2}= BD^{2}+ (8.3/2)^{2}\)
+\((BD)^{2}= (10.3/2)^{2}+ (8.3/2)^{2}\)
+\((BD)^{2}= (10.3/2-8.3/2) (10.3/2+8.3/2) \)
+<p>⇒ \(BD^{2}\) = 9.3<br />
+⇒ \(BD^{2}\) =  √9.3<br />
+Thus, the length of BD is √9.3.<br />
+Step 6: Taking BD as radius and B as centre draw an arc which touches the line segment. The point where it touches the line segment is at a distance of √9.3 from O as shown in the figure.</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+
+
+<p><strong>Question 5:<br/>
+<p>Rationalize</strong><strong> </strong><strong>the denominators of the following:</strong></p>
+<p><strong>(i) 1/√7</strong></p>
+<p><strong>(ii) 1/√7-√6</strong></p>
+<p><strong>(iii) 1/√5+√2</strong></p>
+<p><strong>(iv) 1/√7-2</strong></p>
+<p>&nbsp;</p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<strong>(i)</strong><strong> </strong>\(\frac{1}{\sqrt{7}}=\frac{1\times\sqrt{7} }{1\times\sqrt{7}} = \frac{\sqrt{7}}{7}\)</p>
+<p><strong>(ii) </strong> \(\frac{1}{\sqrt{7}-\sqrt{6}}=\frac{\sqrt{7}+\sqrt{6}}{(\sqrt{7}-\sqrt{6})(\sqrt{7}+\sqrt{6})}\)</p>
+\(=\sqrt{7}+\sqrt{6}\)
+<p>&nbsp;</p>
+<p><strong>(iii)</strong><strong> </strong>\(\frac{1}{\sqrt{5}+\sqrt{2}}=\frac{\sqrt{5}-\sqrt{2}}{(\sqrt{5}+\sqrt{2})(\sqrt{5}-\sqrt{2})}\)</p>
+<p>=\(\frac{\sqrt{5}-\sqrt{2}}{3}\)</p>
+<p>&nbsp;</p>
+<p><strong>(iv)</strong> \(\frac{1}{\sqrt{7}-2}=\frac{(\sqrt{7}+2)}{(\sqrt{7}-2)(\sqrt{7}+2)}\)</p>
+\(=\frac{\sqrt{7}+2}{3}\)
+</div>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 6:<br/>
+<p>Find:</strong><strong><br/>
+<strong>(i) \(64^{\frac{1}{2}}\)</strong></strong></p>
+<p><strong>(ii) \(32^{\frac{1}{5}}\)</strong></p>
+<p><strong> (iii) \(125^{\frac{1}{3}}\)</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p><strong>(i)</strong><strong> </strong>\(64^{\frac{1}{2}}\) = \((2^{6})^{\frac{1}{2}}\) =8</p>
+<p><strong>(ii)</strong><strong> </strong>\(32^{\frac{1}{5}}\) = \((2^{5})^{\frac{1}{5}}\) =2</p>
+<p><strong>(iii)</strong>\(125^{\frac{1}{3}}\) = \((5^{3})^{\frac{1}{3}}\) =5</p>
+</div>
+<p>&nbsp;</p>
+
+
+<p><strong>Question 7:<br/>
+<p><strong>(i) \(9^{\frac{3}{2}}\)</strong></p>
+<p><strong>(ii) \(32^{\frac{2}{5}}\)</strong></p>
+<p><strong>(iii) \(16^{\frac{3}{4}}\)</strong></p>
+<p><strong>(iv) \(125^{\frac{-1}{3}}\)</strong></p>
+<h3 class="azc_tsh_toggle" style="border: 1px solid #cccccc; background-color: #f2f2f2; background-image: none !important; padding-left: 10px; "><a href="#" style="font-size: 14px; 14px">Show Answer</a></h3>
+<div class="azc_tsh_toggle_container" style="border: 1px solid rgb(204, 204, 204); font-size: 14px;margin-left:14px"><br>
+<p>\(9^{\frac{3}{2}}\) = \(3^{3}\) = 27</p>
+<p>\(32^{\frac{2}{5}}\) = \(2^{2}\) =4</p>
+<p>\(16^{\frac{3}{4}}\) = \(2^{3}\) =8</p>
+<p>\(125^{\frac{-1}{3}}\)= \(\frac{1}{125^{\frac{1}{3}}}\)= \(\frac{1}{5}\)</p>
+</div>
+<p>&nbsp;</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div id="disqus_thread"></div>
 <script>
