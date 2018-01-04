6400b8f340e56a172a73fb072a9d67bef54e227b
TPS <tarunprakashiit@gmail.com>
2018-01-04 16:02:30 +0530

    adsense ad added

--- resources/views/blog/layouts/math.blade.php
+++ resources/views/blog/layouts/math.blade.php
@@ -13,7 +13,13 @@
 </script>
 
 
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4438235474748398",
+    enable_page_level_ads: true
+  });
+</script>
 
 
     <meta charset="utf-8">
